9f8e0dea19383d9df38ef17246968d1a12c37baf
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-12-31 01:08:14 +0530

    change our code according to testcase and our test pass

--- ISBNValidator/src/com/learntdd/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/learntdd/isbntools/ValidateISBN.java
@@ -4,35 +4,54 @@ public class ValidateISBN {
 
 	public boolean checkISBN(String isbn) {
 		
-		if(isbn.length() == 13) return true;
-		if(isbn.length() != 10) throw new NumberFormatException("ISBN Number must be 10 digits long");
-		
-		int total = 0;
-		
-		for(int i=0;i<10;i++) {
+		if(isbn.length() == 13) {
 			
-			if(!Character.isDigit(isbn.charAt(i))) {
-				if(i == 9 && isbn.charAt(i) == 'X') {
-					
-					total += 10;
+			int total = 0;
+			for(int i=0;i<13;i++) {
+				if(i%2==0) {
+					total += Character.getNumericValue(isbn.charAt(i));
+				}else {
+					total += Character.getNumericValue(isbn.charAt(i)) * 3;
+				}
+			}
+			
+			if(total % 10 == 0) {
+				return true;
+			}else {
+				return false;
+			}
+			
+		}else {
+			if(isbn.length() != 10) throw new NumberFormatException("ISBN Number must be 10 digits long");
+			
+			int total = 0;
+			
+			for(int i=0;i<10;i++) {
+				
+				if(!Character.isDigit(isbn.charAt(i))) {
+					if(i == 9 && isbn.charAt(i) == 'X') {
+						
+						total += 10;
+						
+					}else {
+						
+						throw new NumberFormatException("ISBN can only contains numeric digits");
+					}
 					
 				}else {
 					
-					throw new NumberFormatException("ISBN can only contains numeric digits");
+					total += Character.getNumericValue(isbn.charAt(i)) * (10-i);
 				}
 				
-			}else {
-				
-				total += Character.getNumericValue(isbn.charAt(i)) * (10-i);
 			}
 			
+			if(total % 11 == 0) {
+				return true;
+			}else {
+				return false;
+			}
 		}
 		
-		if(total % 11 == 0) {
-			return true;
-		}else {
-			return false;
-		}
 	}
 
 }
