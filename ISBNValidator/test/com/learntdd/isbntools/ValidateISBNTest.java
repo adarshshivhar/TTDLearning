cfdf43a27e91e0d6fa3ce2b82481071049580ec9
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-12-30 21:48:17 +0530

    Now here i forget to put @Test annotation that's why second test won't work that's why we always fail first our test to check mistake

--- ISBNValidator/test/com/learntdd/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/learntdd/isbntools/ValidateISBNTest.java
@@ -12,5 +12,11 @@ public class ValidateISBNTest {
 		boolean result = validator.checkISBN(140449116);
 		assertTrue(result);
 	}
+	
+	public void checkInValidISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449116);
+		assertFalse(result);
+	}
 
 }
