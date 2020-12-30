2d99697695a01f88b205b18b369735c71bbb07fc
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-12-30 23:28:39 +0530

    Now all test cases pass for that we write minimal code

--- ISBNValidator/src/com/learntdd/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/learntdd/isbntools/ValidateISBN.java
@@ -3,6 +3,9 @@ package com.learntdd.isbntools;
 public class ValidateISBN {
 
 	public boolean checkISBN(String isbn) {
+		
+		if(isbn.length() != 10) throw new NumberFormatException("ISBN Number must be 10 digits long");
+		
 		int total = 0;
 		
 		for(int i=0;i<10;i++) {
